59839c73b27a9c75f86a1bf5aa38efdf27c031f0
AzGhort <ghort@seznam.cz>
2018-10-29 17:46:32 +0100

    Update perc_update.m

--- perc_update.m
+++ perc_update.m
@@ -2,7 +2,7 @@ function ret = perc_update(p,x,ideal_output,lam)
 ret = p;
 for i=1:size(x,2)
     cur = x(:, i);
-    actual_output = perc_recall(p,cur);
+    actual_output = perc_recall(ret,cur);
     cur=([cur;0])';
     if (actual_output== 0 && ideal_output(i) == 1)
         ret=ret+lam*cur;
